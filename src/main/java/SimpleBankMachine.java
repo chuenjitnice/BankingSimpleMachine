d74a28392fef308322e13c6f478429a9aca062da
chuenjit <chuenjit.vis@gmail.com>
2022-04-03 12:52:49 +0700

    Main to Machine.process and fix option input

--- src/main/java/SimpleBankMachine.java
+++ src/main/java/SimpleBankMachine.java
@@ -5,105 +5,109 @@ import java.util.Scanner;
 
 public class SimpleBankMachine {
     public static void main(String[] args) {
-        Scanner readInput = new Scanner(System.in);
         MachineAction machine = new MachineAction();
-        char option;
-        System.out.print("Input your name: ");
-        String name = readInput.next();
-        machine.CreateAccount(name);
-        machine.ShowWelcome();
-        do{
-            machine.ShowOptions();
-            System.out.print("==========\nEnter option: ");
-            option = readInput.next().charAt(0);
-            double amount = 0;
-            switch (option){
-                case '1':
-                    // Deposit
-                    System.out.print("----------\nEnter amount: ");
-                    amount = readInput.nextDouble();
-                    machine.Deposit(amount);
-                    machine.ShowBalance();
-                    break;
-                case '2':
-                    // Withdraw
-                    System.out.print("----------\nEnter amount: ");
-                    amount = readInput.nextDouble();
-                    machine.Withdraw(amount);
-                    machine.ShowBalance();
-                    break;
-                case '3':
-                    // Check balance
-                    machine.ShowBalance();
-                    break;
-                case '4':
-                    // Transaction history
-                    machine.ShowTransactionHistory();
-                    break;
-                case '5':
-                    // Previous transaction
-                    machine.ShowPreviousTransaction();
-                    break;
-                case '0':
-                    machine.ShowExit();
-                    break;
-                default:
-                    System.out.println("Wrong option. Please try again.");
-                    break;
-            }
-        }while(option != '0');
+        machine.Process();
     }
 }
 class MachineAction{
     private Account account;
 
     public MachineAction() {}
-    public void CreateAccount(String name){
+    public void Process(){
+        Scanner readInput = new Scanner(System.in);
+        String option;
+        System.out.print("Input your name: ");
+        String name = readInput.next();
+        CreateAccount(name);
+        ShowWelcome();
+        do{
+            ShowOptions();
+            System.out.print("==========\nEnter option [0-4]: ");
+            option = readInput.next();
+            double amount = 0;
+            switch (option){
+                case "1":
+                    // Deposit
+                    System.out.print("----------\nEnter amount: ");
+                    amount = readInput.nextDouble();
+                    Deposit(amount);
+                    ShowBalance();
+                    PressEnterToContinue();
+                    break;
+                case "2":
+                    // Withdraw
+                    System.out.print("----------\nEnter amount: ");
+                    amount = readInput.nextDouble();
+                    Withdraw(amount);
+                    ShowBalance();
+                    PressEnterToContinue();
+                    break;
+                case "3":
+                    // Check balance
+                    ShowBalance();
+                    PressEnterToContinue();
+                    break;
+                case "4":
+                    // Transaction history
+                    ShowTransactionHistory();
+                    PressEnterToContinue();
+                    break;
+                case "0":
+                    ShowExit();
+                    break;
+                default:
+                    System.out.println("----------\nWrong option. Please try again.\n----------");
+                    break;
+            }
+        }while(!option.equals("0"));
+    }
+    protected void CreateAccount(String name){
         account = new Account(name);
     }
-    public void ShowOptions(){
+    protected void ShowOptions(){
         System.out.println("==========\nOptions\n==========");
         System.out.println("1. Deposit");
         System.out.println("2. Withdraw");
         System.out.println("3. Check balance");
         System.out.println("4. Transaction history");
-        System.out.println("5. Previous transaction");
         System.out.println("0. Exit");
     }
-    public void Deposit(double amount){
+    protected void Deposit(double amount){
         account.transactionAction(Operation.Credit, amount);
     }
-    public void Withdraw(double amount){
+    protected void Withdraw(double amount){
         account.transactionAction(Operation.Debit, amount);
     }
-    public void ShowPreviousTransaction(){
-        Transaction previousTransaction = account.getPreviousTransaction();
-        if (previousTransaction == null)
-            System.out.println("----------\nNo transaction occur\n----------");
-        else
-            System.out.println("----------\nPrevious transaction: " +previousTransaction.getOperationOnBook()+ " " +previousTransaction.getAmount()+ " Balance: " +previousTransaction.getBalance()+"\n----------");
-    }
-    public void ShowTransactionHistory(){
+    protected void ShowTransactionHistory(){
         Transaction[] transactionHistory = account.getTransactionHistories();
         if (transactionHistory == null)
             System.out.println("----------\nNo transaction occur\n----------");
-        else
-            System.out.println(Arrays.toString(transactionHistory).replace(',','\n'));
+        else {
+            System.out.println("----------\nTransaction History :");
+            System.out.println(" " + Arrays.toString(transactionHistory).replace(',', '\n').replaceAll("[\\[\\]]", ""));
+            System.out.println("----------");
+        }
     }
-    public void ShowWelcome(){
+    protected void ShowWelcome(){
         System.out.println("----------\nWelcome "+account.getName()+"\n----------");
     }
-    public void ShowExit(){
+    protected void ShowExit(){
         System.out.println("----------\nGood bye "+account.getName()+"\n----------");
     }
-    public void ShowBalance(){
+    protected void ShowBalance(){
         System.out.println("----------\nBalance: "+account.getBalance()+"\n----------");
+    }
+    protected void PressEnterToContinue(){
+        System.out.println("Press enter key to back to option menu...");
+        try {
+            System.in.read();
+        }
+        catch (Exception e){}
     }
 }
 class Account{
     private String name;
     private double balance;
-    private Transaction previousTransaction;
     private Transaction[] transactionHistories;
 
     public Account(String name) {
@@ -117,16 +121,12 @@ class Account{
     public double getBalance(){
         return balance;
     }
-    public Transaction getPreviousTransaction() {
-        return previousTransaction;
-    }
     public Transaction[] getTransactionHistories(){
         return transactionHistories;
     }
     public void transactionAction(Operation operation, double amount){
         balance = (operation.equals(Operation.Credit))? balance + amount : balance - amount;
-        previousTransaction = new Transaction(operation, amount, balance);
-        transactionHistories = addTransactionHistory(previousTransaction);
+        transactionHistories = addTransactionHistory(new Transaction(operation, amount, balance));
     }
     private Transaction[] addTransactionHistory(Transaction newTransaction){
         if(transactionHistories == null)
@@ -149,22 +149,13 @@ class Transaction{
         this.balance = balance;
     }
 
-    public Operation getOperation() {
-        return operation;
-    }
     public String getOperationOnBook() {
         return (operation.equals(Operation.Credit)) ? "Deposited" : "Withdrawed";
-    }
-    public double getAmount() {
-        return amount;
-    }
-    public double getBalance() {
-        return balance;
     }
 
     @Override
     public String toString() {
-        return " " + getOperationOnBook() +" "+ amount +" Balance: "+ balance;
+        return getOperationOnBook() +" "+ amount +" Balance: "+ balance;
     }
 }
 enum Operation{
